411c0f187a66cd920ee332100c03293d70a73081
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 14:51:32 +0530

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -2,8 +2,8 @@ import sys
 if len(sys.argv)!=3 :
   print("usage : python script.py <salary> <bonus>")
   exit(1)
-salary=sys.argv[0]
-bonus=sys.argv[1]
+salary=sys.argv[1]
+bonus=sys.argv[2]
 print("salary is ",salary)
 print("bonus is ",bonus)
 total=(salary * bonus/100)+salary
